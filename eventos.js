0b70ffd489b35bda724b30f3d1df01c93a44c550
BeluChanquet <belen.chanquet@gmail.com>
2024-08-11 23:25:30 -0300

    Estilo al div y evento en java

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const botón = document.getElementById('botón');
+
+    botón.addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
