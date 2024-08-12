011ccf6728bf15ec0aff425a67b1b4467886d457
BeluChanquet <belen.chanquet@gmail.com>
2024-08-12 00:52:54 -0300

    Inclusión de stopPropagation

--- eventos.js
+++ eventos.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const botón = document.getElementById('botón');
+    const botón = document.querySelector('button');
+    const divBotón = document.getElementById('botón');
 
-    botón.addEventListener('click', function() {
+    divBotón.addEventListener('click', function() {
         alert('Hola! Soy el div');
+    });
+
+    botón.addEventListener('click', function(event) {
+        event.stopPropagation();
     });
 });
